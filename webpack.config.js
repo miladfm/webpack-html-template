a35a27bf656daaad7eb04715ec60a27ca3f6cd5e
Milad <miladfm@gmail.com>
2021-09-01 14:33:00 +0200

    update packages and use ejs

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   /**
    * Javascript file for each primary pages
    */
@@ -16,22 +17,23 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.html$/,
-        use: [{
-          loader: 'html-loader',
-          options: {
-            attributes: false
-          }
-        }],
-        /**
-         *  Primary pages should be part of exclude list
-         */
-        exclude: [
-          /index.html/,
-          /page1.html/,
-        ]
-      },
+      // {
+      //   test: /\.html$/,
+      //   use: [{
+      //     loader: 'html-loader',
+      //     // options: {
+      //     //   attributes: false
+      //     // }
+      //   }],
+      //   /**
+      //    *  Primary pages should be part of exclude list
+      //    */
+      //   exclude: [
+      //     /index.html/,
+      //     /page1.html/,
+      //   ]
+      // },
+      { test: /\.ejs$/i, use: [ { loader: 'ejs-easy-loader' } ] },
       {
         test: /\.css$/,
         use: [ MiniCssExtractPlugin.loader, "css-loader"],
@@ -50,30 +52,40 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
     port: 3000,
-    overlay: true
+    // overlay: true
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "./css/[name]-[contenthash].bundle.css"
     }),
-    new CopyWebpackPlugin([{
-      from: './assets',
-      to: path.resolve(__dirname, './dist/assets')
-    }]),
+    // new CopyWebpackPlugin([{
+    //   from: './assets',
+    //   to: path.resolve(__dirname, './dist/assets')
+    // }]),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: './assets',
+        to: path.resolve(__dirname, './dist/assets')
+      }],
+    }),
 
     /**
      * Primary pages should load with HtmlWebpackPlugin
      */
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/html/index.html',
-      chunks: ['index']
+      template: './src/html/index.ejs',
+      chunks: ['index'],
+      test: 'Test Duummy Text'
     }),
-    new HtmlWebpackPlugin({
-      filename: 'page1.html',
-      template: './src/html/page1.html',
-      chunks: ['page1']
-    })
+    // new HtmlWebpackPlugin({
+    //   filename: 'page1.html',
+    //   template: './src/html/page1.ejs',
+    //   chunks: ['page1']
+    // })
   ]
 };
